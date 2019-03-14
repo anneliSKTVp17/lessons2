6c64f22e0f124fa882c1f4977e7dc58b49bb784c
user <user@kab305-4>
2019-03-14 20:00:24 +0200

    12.03.19

---
+++ src/Proxy0/RealProject.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proxy0;
+
+/**
+ *
+ * @author user
+ */
+public class RealProject implements Project {
+    private String url;
+    public RealProject(String url) {
+        this.url = url;
+        load();
+    }
+    public void load() {
+        System.out.println("loading project from "+url+"...");
+    }
+
+    @Override
+    public void run() {
+   System.out.println("Running project..." + url + "...");
+    }
+    
+}
